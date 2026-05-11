495813a4a2082efe1c344fd970d41310f8827dd9
Zayden <zaylanthier-lang>
2026-05-11 17:44:18 -0400

    Finished code

--- server/app.py
+++ server/app.py
@@ -10,19 +10,16 @@ existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
 @app.route("/")
 def home():
     """Default route."""
-    return "Welcome to Flatiron Cars!"
+    return "Welcome to Flatiron Cars"
 
 
 @app.route("/<model>")
 def show_model(model):
-    """
-    Dynamic route that checks if a model exists.
-    """
-
+    
     if model in existing_models:
         return f"Flatiron {model} is in our fleet!"
 
-    return f"No models called {model} exists in our catalog."
+    return f"No models called {model} exists in our catalog"
 
 
 if __name__ == "__main__":
